10f7fedbb31c7301c438fa9b2e7118ac97e2a4cd
MariaBusaga <122050757+MariaBussy@users.noreply.github.com>
2024-03-10 20:25:51 +0200

    Add files via upload

---
+++ LAB3_TEMA/WeatherApp/src/main/kotlin/com/sd/laborator/interfaces/WeatherForecastInterface.kt
@@ -0,0 +1,5 @@
+package com.sd.laborator.interfaces
+import com.sd.laborator.pojo.WeatherForecastData
+interface WeatherForecastInterface {
+    fun getForecastData(locationId: Int): WeatherForecastData
+}
